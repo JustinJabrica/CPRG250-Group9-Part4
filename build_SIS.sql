527f6c78d80dcae72785d5ea6dc9c8b8d9d60f19
AliceIvanova <164092366+AliceIvanovaSAIT@users.noreply.github.com>
2024-06-17 14:57:30 -0600

    Update build_SIS.sql

--- build_SIS.sql
+++ build_SIS.sql
@@ -1,1 +1,18 @@
-hey guys.
+DROP TABLE sis_Credential CASCADE CONSTRAINTS;
+DROP TABLE sis_course_within_cred CASCADE CONSTRAINTS;
+DROP TABLE sis_course CASCADE CONSTRAINTS;
+DROP TABLE sis_scheduled_course CASCADE CONSTRAINTS;
+DROP TABLE sis_instructor_course CASCADE CONSTRAINTS;
+DROP TABLE sis_instructor CASCADE CONSTRAINTS;
+DROP TABLE sis_student CASCADE CONSTRAINTS;
+DROP TABLE sis_student_course_record CASCADE CONSTRAINTS;
+DROP TABLE sis_student_Student_Credential CASCADE CONSTRAINTS;
+
+
+rem set date format for 11g
+alter session set NLS_DATE_FORMAT='dd-mon-yy';
+rem set language to english to avoid problems with non-english machines
+alter session set NLS_LANGUAGE = ENGLISH;
+
+SET ECHO ON
+SET FEEDBACK ON
