48fe56713d992d29e5c875c915a44667260d329d
AliceIvanova <164092366+AliceIvanovaSAIT@users.noreply.github.com>
2024-06-17 17:37:05 -0600

    Update build_SIS.sql

--- build_SIS.sql
+++ build_SIS.sql
@@ -44,3 +44,19 @@ CREATE TABLE sis_courses_within_cred
         CONSTRAINT  sis_courses_within_cred_type_flag_format
             CHECK (type_flag IN (0, 1));    
 );
+
+CREATE TABLE sis_scheduled_course
+(
+    CRN NUMBER(5);
+    semester_code CHAR(7);
+    course_code CHAR(7) NOT NULL;
+    section_code CHAR(1) NOT NULL;
+        CONSTRAINT sis_scheduled_course_CRN_pk PRIMARY KEY (CRN),
+        CONSTRAINT sis_scheduled_course_semester_code_pk PRIMARY KEY (semester_code),
+        CONSTRAINT sis_scheduled_course_semester_code_format
+            CHECK (REGEXP_LIKE(semester_code, '^[A-Z]{4}[0-9]{3}$')),    
+        CONSTRAINT sis_scheduled_course_course_code_fk FOREIGN KEY (CRN)
+            REFERENCES (sis_course (course_code)),
+        CONSTRAINT sis_scheduled_course_section_code_format
+            CHECK (REGEXP_LIKE(section_code, '[A-Z]'));
+);
