b23fb73f8f6475f14f75497f2f94b0235c033ce3
AliceIvanova <164092366+AliceIvanovaSAIT@users.noreply.github.com>
2024-06-17 17:45:49 -0600

    Update build_SIS.sql

--- build_SIS.sql
+++ build_SIS.sql
@@ -25,7 +25,7 @@ CREATE TABLE sis_course
     prereq_course_code CHAR(7);
       CONSTRAINT sis_course_course_code_pk PRIMARY KEY(course_code),
       CONSTRAINT sis_course_course_code_ck
-          CHECK (REGEXP_LIKE(course_code, '^[A-Z]{4}[0-9]{3}$')),
+          CHECK (REGEXP_LIKE(course_code, '[A-Z]{4}[0-9]{3}')),
       CONSTRAINT sis_course_prereq_course_code_fk FOREIGN KEY (prereq_course_code)
           REFERENCES (sis_course (course_code));    
 );
@@ -54,7 +54,7 @@ CREATE TABLE sis_scheduled_course
         CONSTRAINT sis_scheduled_course_CRN_pk PRIMARY KEY (CRN),
         CONSTRAINT sis_scheduled_course_semester_code_pk PRIMARY KEY (semester_code),
         CONSTRAINT sis_scheduled_course_semester_code_ck
-            CHECK (REGEXP_LIKE(semester_code, '^[A-Z]{4}[0-9]{3}$')),    
+            CHECK (REGEXP_LIKE(semester_code, '[A-Z]{4}[0-9]{3}')),    
         CONSTRAINT sis_scheduled_course_course_code_fk FOREIGN KEY (CRN)
             REFERENCES (sis_course (course_code)),
         CONSTRAINT sis_scheduled_course_section_code_ck
