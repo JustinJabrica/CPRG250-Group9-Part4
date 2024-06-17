6e1430ebd9b82b6839baea63f6da19c065d87597
AliceIvanova <164092366+AliceIvanovaSAIT@users.noreply.github.com>
2024-06-17 17:22:23 -0600

    Update build_SIS.sql

--- build_SIS.sql
+++ build_SIS.sql
@@ -1,5 +1,5 @@
 DROP TABLE sis_Credential CASCADE CONSTRAINTS;
-DROP TABLE sis_course_within_cred CASCADE CONSTRAINTS;
+DROP TABLE sis_courses_within_cred CASCADE CONSTRAINTS;
 DROP TABLE sis_course CASCADE CONSTRAINTS;
 DROP TABLE sis_scheduled_course CASCADE CONSTRAINTS;
 DROP TABLE sis_instructor_course CASCADE CONSTRAINTS;
@@ -29,3 +29,5 @@ CREATE TABLE sis_course
       CONSTRAINT sis_course_prereq_course_code_fk FOREIGN KEY (prereq_course_code)
           REFERENCES (sis_course (course_code));    
 );
+
+CREATE TABLE sis_courses
