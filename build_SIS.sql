41e2704a514008e406e25c91a237f72d160a2d3b
AliceIvanova <164092366+AliceIvanovaSAIT@users.noreply.github.com>
2024-06-19 14:37:25 -0600

    Update build_SIS.sql

--- build_SIS.sql
+++ build_SIS.sql
@@ -19,10 +19,10 @@ SET FEEDBACK ON
 
 CREATE TABLE sis_course
 (
-    course_code CHAR(7);
-    name VARCHAR2(100) NOT NULL;
-    num_of_credits NUMBER(2,1) NOT NULL;
-    prereq_course_code CHAR(7);
+    course_code CHAR(7),
+    name VARCHAR2(100) NOT NULL,
+    num_of_credits NUMBER(2,1) NOT NULL;,
+    prereq_course_code CHAR(7),
       CONSTRAINT sis_course_course_code_pk PRIMARY KEY(course_code),
       CONSTRAINT sis_course_course_code_ck
           CHECK (REGEXP_LIKE(course_code, '[A-Z]{4}[0-9]{3}')),
@@ -32,9 +32,9 @@ CREATE TABLE sis_course
 
 CREATE TABLE sis_courses_within_cred
 (
-    credential# NUMBER;
-    course_code CHAR(7);
-    type_flag NUMBER(1) NOT NULL;
+    credential# NUMBER,
+    course_code CHAR(7),
+    type_flag NUMBER(1) NOT NULL,
         CONSTRAINT sis_courses_within_cred_credential#_pk PRIMARY KEY(credential#),
         CONSTRAINT sis_courses_within_cred_credential#_fk FOREIGN KEY(credential#)
             REFERENCES (sis_Credential (credential#)),
@@ -42,15 +42,15 @@ CREATE TABLE sis_courses_within_cred
         CONSTRAINT sis_courses_within_cred_course_code_fk FOREIGN KEY(course_code)
             REFERENCES (sis_course (course_code)),
         CONSTRAINT  sis_courses_within_cred_type_flag_ck
-            CHECK (type_flag IN (0, 1));    
+            CHECK (type_flag IN (0, 1))  
 );
 
 CREATE TABLE sis_scheduled_course
 (
-    CRN NUMBER(5);
-    semester_code CHAR(7);
-    course_code CHAR(7) NOT NULL;
-    section_code CHAR(1) NOT NULL;
+    CRN NUMBER(5),
+    semester_code CHAR(7),
+    course_code CHAR(7) NOT NULL,
+    section_code CHAR(1) NOT NULL,
         CONSTRAINT sis_scheduled_course_CRN_pk PRIMARY KEY (CRN),
         CONSTRAINT sis_scheduled_course_semester_code_pk PRIMARY KEY (semester_code),
         CONSTRAINT sis_scheduled_course_semester_code_ck
@@ -58,7 +58,7 @@ CREATE TABLE sis_scheduled_course
         CONSTRAINT sis_scheduled_course_course_code_fk FOREIGN KEY (CRN)
             REFERENCES (sis_course (course_code)),
         CONSTRAINT sis_scheduled_course_section_code_ck
-            CHECK (REGEXP_LIKE(section_code, '[A-Z]'));
+            CHECK (REGEXP_LIKE(section_code, '[A-Z]'))
 );
 
 COMMIT;
